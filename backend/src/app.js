0345350fe2e33ab1c12d8b01d1bb146ad2350993
SachinSharma22 <s830sharma@gmail.com>
2026-04-19 09:17:25 +0530

    feat: backend completed with successful response from api

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import aiRoutes from "./routes/ai.routes.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req,res) => {
     res.send("Maar ke kha jaao part 2");
 });
--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -14,6 +14,11 @@ const ai = new GoogleGenAI({
 export default async function aiService(prompt) {
   const response = await ai.models.generateContent({
     model: "gemini-3-flash-preview",
+    systemInstruction: `
+      You are a code reviewer, who have expertise in development. You look for the code and find the problem and suggest the solution to the developer.
+
+      You always try to find the best solution for the developer and also try to make the code more effecient and clean.
+    `,
     contents: prompt,
   });
 
